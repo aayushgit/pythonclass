e181f33d35fcffbb06a792f2e8a8b12606749f39
Aayush Sharma <sharmagiriaayush@gmail.com>
2018-01-15 13:02:57 +0545

    Export to CSV added

--- webscrape/kaymu_mobile.py
+++ webscrape/kaymu_mobile.py
@@ -17,14 +17,15 @@ uClient.close()
 page_soup = soup(page_html,"html.parser")
 #get the container of phones
 container=page_soup.findAll("div",{"class":"sku -gallery "})
+
+filename = "products.csv"
+f = open(filename,"w")
+headers="brand,product_name,discount_rate,price\n"
+f.write(headers)
 for contain in container:
 	brand = contain.h2.span.text.strip()
 	name = contain.div.img["alt"]
 	price_container = contain.findAll("div",{"class":"price-container"})
 	discount = price_container[0].span.text.strip()
 	price = price_container[0].findAll("span",{"class":"price"})[0].text.strip()
-
-	print("Brand:" +brand)
-	print("Name:" +name)
-	print("Discount:" +discount)
-	print("Price:" +price)
+	f.write(brand.replace(",","|") + "," +name.replace(",","|")+ "," +discount+ "," +price.replace(","," ") + "\n")
