fa65dee628f26bfabde464b560f9b6fb867f214a
Aayush Sharma <sharmagiriaayush@gmail.com>
2018-01-16 16:16:19 +0545

    Download Youtube Videos App

---
+++ webscrape/youtube/youtubedownload.py
@@ -0,0 +1,26 @@
+import requests
+import urllib.parse as ul
+
+def download_file(url):
+	local_filename = 'download.mp4'
+	r = requests.get(url, stream=True)
+	with open(local_filename, 'wb') as f:
+		for chunk in r.iter_content(chunk_size=1024):
+			if chunk:
+				f.write(chunk)
+	return (local_filename)
+
+a = requests.get('http://youtube.com/get_video_info?video_id=xk0h_DkEyYE')
+parsed = ul.parse_qs(a.text)
+parsed =  parsed['url_encoded_fmt_stream_map']
+new = str(parsed).split(',')
+#for i in new:  See all the video quality available
+#		print (i)
+#		print ("\n")
+
+ed = ul.parse_qs(new[0]) #create dictionary of the given URL
+#print (ed)
+for i in ed: #get all dictionary elements
+	if 'url' in i:
+		download_file (ed[i][0])
+
