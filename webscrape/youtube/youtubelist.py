4783e691d7b92e149e7c60450c29fbbb3b628f25
Sanip Rijal <saneprijal@gmail.com>
2018-01-16 11:59:47 +0545

    URL Changed and added RE to match the video URL

--- webscrape/youtube/youtubelist.py
+++ webscrape/youtube/youtubelist.py
@@ -1,14 +1,20 @@
 import urllib.request
 from bs4 import BeautifulSoup as soup
+
+#imported re package
+import re
+
 class AppURLOpener(urllib.request.FancyURLopener):
 	version = "Mozilla/5.0"
 opener = AppURLOpener()
 
-my_url = "https://www.youtube.com/playlist?list=PLY_geU-dV3mb3BEFXsUATnspnttym9N6Cclass"
+my_url = "https://www.youtube.com/playlist?list=PLY_geU-dV3mb3BEFXsUATnspnttym9N6C" #changed the url slightly by removing the 'class' written at last
 uClient = opener.open(my_url)
 html_page = uClient.read()
 uClient.close()
 page_soup = soup(html_page,"html.parser")
-songlist = page_soup.findAll
+
+songlist = page_soup.find_all(href = re.compile('^/watch.*')) #capture the video url from the html page
+
 for l in songlist:
 	print (l.get("href"))
